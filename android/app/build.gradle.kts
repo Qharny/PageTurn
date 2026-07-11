c10ff3ff56099403fb5c8e6f02ad29efb1488071
qharny <kabuteymanasseh5@gmail.com>
2026-07-11 17:47:15 +0000

    feat: add android app build.gradle.kts configuration for Flutter project

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.example.pageturn"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
